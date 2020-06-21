3750c3510ed6c2e4cd15b337bf7c05f8917669ed
Jesse Isleib <jesse.isleib@gmail.com>
2020-06-20 18:20:29 -0700

    initial commit

---
+++ read.py
@@ -0,0 +1,11 @@
+import scipy
+from scipy.io.wavfile import read
+import matplotlib.pyplot as plt
+
+input_ = read("output.wav")
+audio = input_[1]
+plt.plot(audio[0:1024])
+plt.ylabel("dummyx")
+plt.xlabel("dummyy")
+plt.title("dummytitle")
+plt.show()
