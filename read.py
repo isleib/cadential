8c9e95d41a155041a0306100789e0766336079ec
Jesse Isleib <jesse.isleib@gmail.com>
2020-06-23 19:13:49 -0700

    chang plot size and increase value of variable names

--- read.py
+++ read.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 input_ = read("output.wav")
 audio = input_[1]
-plt.plot(audio[0:1024])
+plt.plot(audio[0:1048576])
 plt.ylabel("dummyx")
 plt.xlabel("dummyy")
 plt.title("dummytitle")
